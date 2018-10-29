0c04aa8aae3f1bcbb5b0fbfc37fccc4345c1cfc6
Horpey <adeniran1059@gmail.com>
2018-10-29 18:09:18 +0100

    Restructure

--- static/sw-push-notification.js
+++ static/sw-push-notification.js
@@ -1,41 +1,21 @@
-// if you send a push notification from server, it might be instant or delay up 10 minutes
-// https://developer.mozilla.org/en-US/docs/Web/API/PushEvent
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  // push notification can send event.data.json() as well
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+console.log("Service Worker Loaded...");
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
-  const title = 'MyApp Alert';
-  const options = {
-    body: event.data.text(),
-    icon: '/static/img/icon-512.png',
-    badge: '/static/img/icon-96.png',
-    tag: 'alert'
-  };
+self.addEventListener("push", e => {
+  const data = e.data.json();
+    console.log(JSON.stringify(data) );
+  console.log("Push Recieved...");
 
-  event.waitUntil(self.registration.showNotification(title, options));
+  self.registration.showNotification(data.title, {
+    body: data.body
+    // icon: data.icon ,
+    // action: "index.html", //data.url
+
+
+  });
 });
 
 
-self.addEventListener('notificationclick', function(event) {
-  // can handle different type of notification based on event.notification.tag
-  console.log(`[Service Worker] Notification click Received: ${event.notification.tag}`);
 
-  event.notification.close();
-
-  // Modify code from https://developer.mozilla.org/en-US/docs/Web/API/WindowClient/focus
-  // find existing "/notification" window to focus on, or open a new one if not available
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    const client = clientList.find(function(c) {
-      new URL(c.url).pathname === '/notification'
-    });
-    if (client !== undefined) {
-      return client.focus();
-    }
-
-    return clients.openWindow('/notification');
-  }));
-});
+//setTimeout(n.close(), 1 * 1000)
+//n.close()
+//const n = new Notification('Hey')
